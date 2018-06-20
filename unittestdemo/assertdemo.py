0ee24b3b27f7b1510795d664c439cbbd837200b4
anton <babos77@gmail.com>
2018-06-20 11:04:39 +0300

    Assertions updates

--- unittestdemo/assertdemo.py
+++ unittestdemo/assertdemo.py
@@ -6,12 +6,13 @@ class AssertDemo(unittest.TestCase):
     def test_assertTrueFalse(self):
         a = True
         self.assertTrue(a, "a is not true")
+        # self.assertFalse(a, "a is not false")
         b = False
         self.assertFalse(b, "b is not false")
 
     def test_assertEqual(self):
         a = "QA"
-        b = "None"
+        b = "QA"
         self.assertEqual(a, b, msg="a is not equal to b")
 
 
